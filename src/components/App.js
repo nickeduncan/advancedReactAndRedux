7d99124fe7d424b31dc320169b1b16eaa1482452
Nicholas Duncan <nickeduncan@gmail.com>
2018-06-21 16:27:34 -0400

    [2-71] added state to renderButton and renderHeader

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-export default class App extends Component {
-	renderButton() {}
+class App extends Component {
+	renderButton() {
+		if (this.props.auth) {
+			return <button>Sign Out</button>;
+		} else {
+			return <button>Sign In</button>;
+		}
+	}
+
 
 	renderHeader() {
 		return (
@@ -23,9 +31,16 @@ export default class App extends Component {
 	render() {
 		return (
 			<div>
+				{this.renderHeader()}
 				<Route path="/post" component={CommentBox} />
 				<Route exact path="/" component={CommentList} />
 			</div>
 		);
 	}
 };
+
+function mapStateToProps(state) {
+	return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(App);
