1edede81ef743c967d1bc497cc0ab0d0076da24e
Nicholas Duncan <nickeduncan@gmail.com>
2018-06-21 19:18:31 -0400

    [4-82] checked if action has a payload and a next property

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,3 +1,10 @@
 export default ({ dispatch }) => next => action => {
-
-}
+  // Check to see if the aciton 
+	// has a promise on its 'payload' property
+	// If it does, then wait for it to resolve
+	// if it doesn't, then send the aciotn on to the
+	// next middlware
+  if (!action.payload || !action.payload.then) {
+		return next(action);
+	}
+};
