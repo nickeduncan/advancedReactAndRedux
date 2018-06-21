8240c43b2b5e53f588f2612d6450f92068fac3d5
Nicholas Duncan <nickeduncan@gmail.com>
2018-06-21 10:31:23 -0400

    [2-37] added describe statement to CommentBox test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,22 +17,22 @@ it('has a text area and a button', () => {
 	expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-	wrapped.find('textarea').simulate('change', {
-	  target: { value: 'new comment' }
+describe('the text area', () => {
+  beforeEach(() => {
+		wrapped.find('textarea').simulate('change', {
+			target: { value: 'new comment' }
+		});
+		wrapped.update();
 	});
-	wrapped.update();
 
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-});
 
-it('has a text area that clears after submit', () => {
-	wrapped.find('textarea').simulate('change', {
-	  target: { value: 'new comment' }
+	it('users can type in', () => {
+		expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 	});
-	wrapped.update();
 
-	wrapped.find('form').simulate('submit');
-	wrapped.update();
-  expect(wrapped.find('textarea').prop('value')).toEqual('');
+	it('clears after submit', () => {
+		wrapped.find('form').simulate('submit');
+		wrapped.update();
+		expect(wrapped.find('textarea').prop('value')).toEqual('');
+	});
 });
