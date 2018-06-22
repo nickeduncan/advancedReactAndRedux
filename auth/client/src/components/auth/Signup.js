6d829a23b1e2f71cff82020ef7a5d6b7fb143d31
Nicholas Duncan <nickeduncan@gmail.com>
2018-06-22 12:17:48 -0400

    [6-133] display error message on singup

--- auth/client/src/components/auth/Signup.js
+++ auth/client/src/components/auth/Signup.js
@@ -32,11 +32,18 @@ class Signup extends Component {
 						autoComplete="none"
 					/>
 				</fieldset>
+				<div>
+					{this.props.errorMessage}
+				</div>
 				<button>Sign Up!</button>
 			</form>
 		);
 	}
 }
 
-export default compose(connect(null, actions), reduxForm({ form: 'signup' })
+function mapStateToProps(state) {
+	return { errorMessage: state.auth.errorMessage };
+}
+
+export default compose(connect(mapStateToProps, actions), reduxForm({ form: 'signup' })
 )(Signup);
