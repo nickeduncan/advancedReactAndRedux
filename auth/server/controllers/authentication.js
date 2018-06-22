286dd15a145abd56ac67db35a6cf84bac0bea021
Nicholas Duncan <nickeduncan@gmail.com>
2018-06-21 21:47:10 -0400

    [5-102] added logic to create user or return error if email in db

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -1,4 +1,4 @@
-import User = require('../models/user');
+User = require('../models/user');
 
 exports.signup = function(req, res, next) {
 	const email = req.body.email;
@@ -6,12 +6,23 @@ exports.signup = function(req, res, next) {
 
 	// See if a user with the given email exists
 	User.findOne({ email: email }, function(error, existingUser) {
-	
-	});
 
-	// If a user with email does exist, return an error
-	
-	// If a user with email does NOT exist, create and save user record
-	
-	// Respond to request indicating the user was created
+		// If a user with email does exist, return an error
+		if (existingUser) {
+			return res.status(422).send({ error: 'Email is in use' });
+		}
+		
+		// If a user with email does NOT exist, create and save user record
+		const user = new User({
+			email: email,
+			password: password
+		});
+		
+		user.save(function(err) {
+			if (err) { return next(err); }
+		
+			// Respond to request indicating the user was created
+			res.json({ success: true });
+		});
+	});
 }
--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
 	email: { type: String, unique: true, lowercase: true },
 	password: String
-}
+})
 
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema);
