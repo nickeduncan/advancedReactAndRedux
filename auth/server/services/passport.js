fb7397e805390f43af76e30efe797b62a48824bc
Nicholas Duncan <nickeduncan@gmail.com>
2018-06-21 22:48:34 -0400

    [5-114] implemented middleware to keep users from signing in without saved email/password

--- auth/server/services/passport.js
+++ auth/server/services/passport.js
@@ -16,9 +16,13 @@ const localLogin = new LocalStrategy( localOptions, function(email, password, do
 	  if (!user) { return done(null, false); }
 		
 		// compare passwords - is `password` equal to user password?
-		
-	});
+		user.comparePassword(password, function(err, isMatch) {
+			if (err) { return done(err); }
+			if (!isMatch) { return done(null, false); }
 
+			return done(null, user);
+		});
+	});
 });
 
 // Setup options for JWT strategy
@@ -45,3 +49,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 
 // tell passport to use this strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
